16dfd207868de2cdc8b14c67c7f09063664bba78
echoedinvoker <echoedinvoker@gmail.com>
2022-09-15 14:24:14 +0800

    aaa

--- 17-Modern-JS-Modules-Tooling/275-CommonJS module/script.js
+++ 17-Modern-JS-Modules-Tooling/275-CommonJS module/script.js
@@ -1,10 +1,10 @@
 // Export
-export.addToCart = function(product, quantity) {
-    createImageBitmap.push({ product, quantity })
-    console.log(
-        `${quantity} ${product} added to cart`
-    );
-}
+// export.addToCart = function(product, quantity) {
+//     createImageBitmap.push({ product, quantity })
+//     console.log(
+//         `${quantity} ${product} added to cart`
+//     );
+// }
 
 // Import
 const { addToCart } = require("./shoppingCart.js");
